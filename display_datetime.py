a71ccd7d539eea3b872dda810bdbfa60790e264e
Muhammed Ali <muhamzyali@gmail.com>
2021-05-13 23:50:31 +0100

    display date

--- display_datetime.py
+++ display_datetime.py
@@ -1,5 +1,7 @@
 from delorean import Delorean   #importing the  library  we installed previously
 
-dt_tm = Delorean().date #Function that carries all the date and time properties
+dt_tm = Delorean() #Function that carries all the date and time properties
 print ("date and time = ",dt_tm)
 
+dt = Delorean().date 
+print ("date = ",dt)
