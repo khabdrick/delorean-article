18d2679bf79fccf30e185095c4b548db5d17fc86
Muhammed Ali <muhamzyali@gmail.com>
2021-05-14 00:34:42 +0100

    display timezone

--- display_datetime.py
+++ display_datetime.py
@@ -1,13 +1,12 @@
 from delorean import Delorean   #importing the  library  we installed previously
 
 dt_tm = Delorean() #Function that carries all the date and time properties
-# print ("date and time = ",dt_tm)
-
-dt = Delorean().date 
-print ("date = ",dt)
-
-tm = Delorean().datetime.time()
-print ("time = ",tm)
-
 change_tz = dt_tm.shift('Africa/Lagos')
 print ("date and time = ",dt_tm)
+
+dt = change_tz.date 
+print ("date = ",dt)
+
+tm = change_tz.datetime.time()
+print ("time = ",tm)
+
