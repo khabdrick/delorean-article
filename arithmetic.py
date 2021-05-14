4b8749f3c0027e8f0f96ab3622f07032d9290fdb
Muhammed Ali <muhamzyali@gmail.com>
2021-05-14 23:28:14 +0100

    docs

--- arithmetic.py
+++ arithmetic.py
@@ -4,10 +4,11 @@ from datetime import timedelta
 dt_tm = Delorean()
 dt_tm += timedelta(days=5)
 
-print(dt_tm)
+print("Date 3 days from now is ", dt_tm)
 
 
 dt_tm_ago = Delorean()
 dt_tm_ago -= timedelta(days=3)
 
 print("Date 3 days ago is ", dt_tm_ago)
+
