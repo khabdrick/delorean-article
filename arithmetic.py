e15928c75298bd00433f37c6912f100313437cf0
Muhammed Ali <muhamzyali@gmail.com>
2021-05-14 23:32:44 +0100

    childrens day arithmetic|

--- arithmetic.py
+++ arithmetic.py
@@ -1,14 +1,16 @@
 from delorean import Delorean
-from datetime import timedelta 
+import datetime
 
 dt_tm = Delorean()
-dt_tm += timedelta(days=5)
+dt_tm += datetime.timedelta(days=5)
 
 print("Date 3 days from now is ", dt_tm)
 
 
 dt_tm_ago = Delorean()
-dt_tm_ago -= timedelta(days=3)
+dt_tm_ago -= datetime.timedelta(days=3)
 
 print("Date 3 days ago is ", dt_tm_ago)
 
+
+ch_day = Delorean(datetime(2021, 5, 27))
