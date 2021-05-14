6b3f504d9a025c58128edc022cd21bc0182a0e10
Muhammed Ali <muhamzyali@gmail.com>
2021-05-14 22:51:07 +0100

    past date

--- arithmetic.py
+++ arithmetic.py
@@ -2,6 +2,12 @@ from delorean import Delorean
 from datetime import timedelta 
 
 dt_tm = Delorean()
-dt_tm += timedelta(days=3)
+dt_tm += timedelta(weeks=5)
 
 print(dt_tm)
+
+
+dt_tm_ago = Delorean()
+dt_tm_ago -= timedelta(days=3)
+
+print("Date 3 days ago is ", dt_tm_ago)
