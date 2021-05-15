825e51733d784d1750d076a03bf7bcf4d5cbecf5
Muhammed Ali <muhamzyali@gmail.com>
2021-05-16 00:39:24 +0100

    update display

--- display_datetime.py
+++ display_datetime.py
@@ -1,6 +1,16 @@
 from delorean import Delorean   #importing the  library  we installed previously
 
 dt_tm = Delorean() #Function that carries all the date and time properties
+
+print ("date = ",dt_tm)  
+
+tm = Delorean().datetime.time()  #new
+print ("time = ",tm)  #new
+
+#display naive datetime
+naive_dt_tm = dt_tm.naive #new
+print ("Naive datetime is",naive_dt_tm)  #new
+
 change_tz = dt_tm.shift('Africa/Lagos')
 print ("date and time = ",dt_tm)
 
@@ -10,9 +20,7 @@ print ("date = ",dt)
 tm = change_tz.datetime.time()
 print ("time = ",tm)
 
-#display naive datetime
-naive_dt_tm = dt_tm.naive #new
-print ("Naive datetime is",naive_dt_tm)  #new
+
 
 nxt_fri = dt_tm.next_friday()
 print ("date for next week friday is = ", nxt_fri)
