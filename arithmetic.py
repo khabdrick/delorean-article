2b526b0581996b13cb8e75dd0ec16707aa108a45
Muhammed Ali <muhamzyali@gmail.com>
2021-05-15 08:08:44 +0100

    alter time and date

--- arithmetic.py
+++ arithmetic.py
@@ -18,3 +18,8 @@ days_till_ch_day = ch_day - present_date
 print("Days till children day ", days_till_ch_day)
 print("Days till children day ",days_till_ch_day.days)  #display only the number of  days
 
+d = Delorean(datetime=datetime.datetime(2021, 5, 14, 12), timezone='UTC')
+altered_tm = d.replace(hour=10)
+altered_dt = d.replace(month=10)
+print("Altered time is ", altered_tm)
+print("Altered date is ", altered_dt)
