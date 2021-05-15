48f9061992f03d4ce8ce368a485cf7128b907a08
Muhammed Ali <muhamzyali@gmail.com>
2021-05-15 22:30:25 +0100

    clean up serializer

--- serializer.py
+++ serializer.py
@@ -1,8 +1,11 @@
 from delorean import Delorean
+import json
 
 d = Delorean().datetime
 print("Normal datetime value", d, "Type: ", type(d))
+
 d_string = d.isoformat()
 print ("serialized datetime value", d_string, "Type: ", type(d_string))
+
 d_str = str(d)
 print("serialized datetime value with str method", d_str, "Type: ", type(d_str))
